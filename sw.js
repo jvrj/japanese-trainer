1b2028303fc871ed8362e60a8be7ddc63cec5d88
jvrj <juliuspireh@gmail.com>
2026-04-15 16:44:45 +1000

    initial deploy

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'jp-trainer-v1';
+const urlsToCache = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
